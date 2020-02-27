1e9d0848244cf627ebaac9c989bfe44cdd22a4be
feige <feige84@126.com>
2020-02-27 13:50:46 +0800

    增加正则匹配

--- string.go
+++ string.go
@@ -1,5 +1,7 @@
 package utils
 
+import "regexp"
+
 func SubStr(str string, start int, length int) string {
 	rs := []rune(str)
 	rl := len(rs)
@@ -32,4 +34,47 @@ func SubStr(str string, start int, length int) string {
 func SubStr2(str string, start int, end int) string {
 	rs := []rune(str)
 	return string(rs[start:end])
+}
+
+// MatchOneOf match one of the patterns
+func MatchOneOf(text string, patterns ...string) []string {
+	var (
+		re    *regexp.Regexp
+		value []string
+	)
+	for _, pattern := range patterns {
+		// (?flags): set flags within current group; non-capturing
+		// s: let . match \n (default false)
+		// https://github.com/google/re2/wiki/Syntax
+		re = regexp.MustCompile(pattern)
+		value = re.FindStringSubmatch(text)
+		if len(value) > 0 {
+			return value
+		}
+	}
+	return nil
+}
+
+// MatchAll return all matching results
+func MatchAll(text, pattern string) [][]string {
+	re := regexp.MustCompile(pattern)
+	value := re.FindAllStringSubmatch(text, -1)
+	return value
+}
+
+// MatchOneOf match one of the patterns
+func MatchBool(text string, patterns ...string) bool {
+	var (
+		re *regexp.Regexp
+	)
+	for _, pattern := range patterns {
+		// (?flags): set flags within current group; non-capturing
+		// s: let . match \n (default false)
+		// https://github.com/google/re2/wiki/Syntax
+		re = regexp.MustCompile(pattern)
+		if re.MatchString(text) {
+			return true
+		}
+	}
+	return false
 }
